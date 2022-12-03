1f3420ae7a75164f726f5dc403f990f1c6af8203
RamonCarella <ramoncarella43@gmail.com>
2022-12-03 15:12:43 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 from tkinter import *
 
 root = Tk()
+root.geometry('600x400')
+
+headingLabel = Label(root, text="Meteo App from Ramon&Braga o Braga&Ramon")
+headingLabel.pack()
+
 
 myLabel = Label(root, text="rrcamadonna")
 myLabel.pack()
