98f3f163d020d9fa828bdffec24c8131453006b3
RamonCarella <ramoncarella43@gmail.com>
2022-12-03 15:20:52 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from tkinter import *
+import requests
+import json
 
 root = Tk()
 root.geometry('600x400')
+
+root.title("Meteo App from Ramon&Braga o Braga&Ramon")
+root.resizable(0,0) 
+
 
 headingLabel = Label(root, text="Meteo App from Ramon&Braga o Braga&Ramon")
 headingLabel.pack()
@@ -9,5 +15,11 @@ headingLabel.pack()
 
 myLabel = Label(root, text="rrcamadonna")
 myLabel.pack()
+
+canvas1 = Canvas(root, width=600, height=400)
+canvas1.pack()
+entry1 = Entry(root)
+canvas1.create_window(200, 30, window=entry1)
+
 
 root.mainloop()
